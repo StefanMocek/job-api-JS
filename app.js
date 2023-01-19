e7fc4d48484ed0218be8d7cb0eb2a9e1cca789dd
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-01-19 12:59:22 +0100

    procfile and fixes

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("job API")
+})
 
 app.use('/api/v1/auth', authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
